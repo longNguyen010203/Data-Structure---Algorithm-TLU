58eb84666732b81928babf2f9ebbf56598606979
Long Nguyen <zedsc03@gmail.com>
2024-07-16 16:22:07 +0700

    Update course

--- Week-1/main.cpp
+++ Week-1/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 using namespace std;
 
 
-int main(int a, int b) { return a + b; }
+int ROWS = 3;
+int COLUMNS = 3;
+
+
+int main() {
+
+    int matrix[ROWS][COLUMNS] = {
+        {1, 2, 3}, {4, 5, 6}, {7, 8, 4}
+    };
+
+    for (int row = 0; row < ROWS; row++) {
+        for (int column = 0; column < COLUMNS; column++) {
+            cout << matrix[row][column] << " ";
+        } cout << endl;
+    }
+
+    return 0;
+}
