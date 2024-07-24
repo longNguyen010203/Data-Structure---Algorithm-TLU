d1885c16c3db7e8facd16e91cf5dace3f79ae7a6
Long Nguyen <zedsc03@gmail.com>
2024-07-24 23:28:58 +0700

    Add exercise-5 of week2

---
+++ Week-2/exercise-5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+
+/*
+    Biểu thức Modulo
+*/
+
+
+int main() {
+
+    unsigned int n, m; cin >> n >> m;
+    unsigned long long k = 1;
+
+    for (int i = 0; i < n; i++) {
+        k = k * 2;
+    }
+    cout << k << endl;
+    // cout << m % k;
+    cout << 98765432 / 4;
+    
+
+    return 0;
+}
