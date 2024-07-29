abd011dd9371c3dd052df215d3b38fef9ac0742f
Long Nguyen <zedsc03@gmail.com>
2024-07-29 22:45:05 +0700

    Update

--- Week-2/exercise-3.cpp
+++ Week-2/exercise-3.cpp
@@ -3,33 +3,33 @@ using namespace std;
 
 
 /*
-    Kinh doanh thật dễ 3
+    Kinh doanh thật dễ 3 ====> O(n)
 */
 
 
 int main() {
 
-    unsigned int n; cin >> n;
-    short a[n], max, min;
+    unsigned int n; cin >> n; // O(1)
+    short a[n], max, min; // O(1)
 
     for  (int i = 0; i < n; i++) {
         cin >> a[i];
-    }    
+    } // ==> O(n)
 
-    max = a[0];
-    for (int i = 0; i < n; i++) {
-        if (max < a[i]) { max = a[i]; }
-    }
+    max = a[0]; // O(1)
+    for (int i = 0; i < n; i++) { // O(n)
+        if (max < a[i]) { max = a[i]; } // O(1)
+    } // ==> O(n)
 
-    min = a[0];
-    for (int i = 0; i < n; i++) {
-        if (min > a[i]) { min = a[i]; }
-    }
+    min = a[0]; // O(1)
+    for (int i = 0; i < n; i++) { // O(n)
+        if (min > a[i]) { min = a[i]; } // O(1)
+    } // ==> O(n)
 
-    cout << max-min;
+    cout << max-min; // O(1)
 
-    return 0;
-}   
+    return 0; // O(1)
+} 
 
 
 
