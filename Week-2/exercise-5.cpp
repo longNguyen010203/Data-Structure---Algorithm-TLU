041991929f8eef5b5fdefeccfa40cbc35997fac8
Long Nguyen <zedsc03@gmail.com>
2024-07-29 22:45:26 +0700

    Update

--- Week-2/exercise-5.cpp
+++ Week-2/exercise-5.cpp
@@ -3,27 +3,28 @@ using namespace std;
 
 
 /*
-    Số may mắn
+    Số may mắn ===> O(n)
 */
 
 
  
 int main()
 {
-    unsigned long long n; cin >> n;
-    unsigned short du, count = 0;
-    while (n != 0) {
-        du = n % 10;
-        n = n / 10;
-        if (du == 4 || du == 7) {
-            count++;
+    unsigned long long n; cin >> n; // O(1)
+    unsigned short du, count = 0; // O(1)
+    while (n != 0) { // O(n)
+        du = n % 10; // O(1)
+        n = n / 10; // O(1)
+        if (du == 4 || du == 7) { // O(1)
+            count++; // O(1)
         }
-    }
-    if (count == 4 || count ==7) {
-        cout << "YES";
+    } // ==> O(n)
+
+    if (count == 4 || count ==7) { // O(1)
+        cout << "YES"; // O(1)
     } else {
-        cout << "NO";
+        cout << "NO"; // O(1)
     }
     
-    return 0;
+    return 0; // O(1)
 }
