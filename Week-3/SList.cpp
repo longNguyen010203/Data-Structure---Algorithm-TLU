0a1fd68d40ca10d9f0d4652cc4af7d8ba12c8cbb
Long Nguyen <zedsc03@gmail.com>
2024-07-31 23:40:19 +0700

    Update SList

--- Week-3/SList.cpp
+++ Week-3/SList.cpp
@@ -23,7 +23,7 @@ class SList {
         int getSize() const { return size; } // Lấy ra kích thước của list
 
         void print() const {
-            if (isEmpty()) { throw "List is Empty!"; }
+            if (isEmpty()) { cout << "List is Empty!"; }
             else {
                 Node<E>* current = head;
                 while (current != NULL) {
@@ -63,11 +63,11 @@ class SList {
             size++;
         }
 
-        // Thêm một phần tử vào vị trí i
+        // Thêm một Node vào vị trí i
         void add (int i, const E &data) {
-            if (i <= 0 || i > getSize()+1) { throw "Out of range"; }
-            if (i == 1) { addFirst(data); size++; return;}
-            else if (i == getSize()+1) { addLast(data); size++; return; } 
+            if (i <= 0 || i > getSize()+1) { cout << "Out of range"; return; }
+            if (i == 1) { addFirst(data); size++;}
+            else if (i == getSize()+1) { addLast(data); size++; } 
             else {
                 Node<E>* current = head;
                 Node<E>* tmp = makeNode(data);
@@ -82,29 +82,59 @@ class SList {
 
         // Xóa phần tử đầu
         void removeFirst() {
-            if (isEmpty()) { throw "List is Empty!"; }
+            if (isEmpty()) { cout << "List is Empty!"; return; }
+            if (getSize() == 1) {head = NULL; size--; return;}
             head = head->next;
             size--;
         }
 
         // Xóa phần tử cuối
-        
+        void removeLast() {
+            if (isEmpty()) { cout << "List is Empty!"; return; }
+            if (getSize() == 1) {head = NULL; size--; return;}
+            Node<E>* current = head;
+            for (int i = 1; i < size-1; i++) {
+                current = current->next;
+            }
+            current->next = NULL;
+            size--;
+        }
+
+        // Xóa một Node tại vị trí i
+        void remove(int i) {
+            if (isEmpty()) { cout << "List is Empty!"; return; }
+            if (i <= 0 || i > getSize()) { cout << "Out of range"; return; }
+            if (i == 1) { removeFirst(); size--; }
+            else if (i == getSize()) { removeLast(); size--; }
+            else {
+                Node<E>* begin = head;
+                Node<E>* back = head->next;
+                for (int j = 1; j < i-1; j++) {
+                    begin = begin->next;
+                    back = back->next;
+                }
+                begin->next = back->next;
+                back->next = NULL;
+                size--;
+            }
+        }
+
 };
 
 
 int main() {
 
     SList<int> list;
-    list.addFirst(20);
-    list.addFirst(30);
     list.addFirst(40);
+    list.addFirst(30);
+    list.addFirst(20);
     list.addLast(50);
-    list.add(2, 100);
-    list.removeFirst();
-    list.addFirst(1);
+    list.add(3, 35);
     list.print();
     cout << endl;
-    cout << list.getSize();
-
+    list.removeFirst();
+    list.removeLast();
+    list.remove(2);
+    list.print();
     return 0;
 }
